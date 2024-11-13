40a6a9b07701a5a468c62aba92651667d84f5ece
devworlds <jeffluizblockchain@gmail.com>
2024-11-13 17:04:16 -0300

    feat: update comments

--- internal/ethereum/methods.go
+++ internal/ethereum/methods.go
@@ -8,19 +8,20 @@ import (
 	"github.com/ethereum/go-ethereum/crypto"
 )
 
-// GenerateEvmWallet creates a new Ethereum wallet and returns the address and store private key in memory.
+// GenerateWallet creates a new Ethereum wallet and returns the address, private key, and public key.
 func GenerateWallet() (Wallet, error) {
-	// Generate a new private key
+
+	// Generate a new private key from ECDSA
 	privateKey, err := crypto.GenerateKey()
 	if err != nil {
 		return Wallet{}, fmt.Errorf("failed to generate private key: %v", err)
 	}
 
-	// Extract the public key from the private key
+	// Convert the private key to bytes and hex
 	privateKeyBytes := crypto.FromECDSA(privateKey)
 	privateKeyHex := hex.EncodeToString(privateKeyBytes)
 
-	// Generate the public key from the private key
+	// Get the public key from the private key
 	publicKeyECDSA := privateKey.Public().(*ecdsa.PublicKey)
 	publicKeyBytes := crypto.FromECDSAPub(publicKeyECDSA)
 	publicKeyHex := hex.EncodeToString(publicKeyBytes)
